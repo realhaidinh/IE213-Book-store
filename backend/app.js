c826a0d95262f864121045ff9126fea450506bae
Hai Dinh <dinhnguyenminhhai@gmail.com>
2024-04-20 21:42:32 +0700

    dotenv config

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,10 @@ const fileUpload = require("express-fileupload");
 
 const path = require("path");
 
-
+require("dotenv").config({ path: "backend/config/config.env" });
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(fileUpload());
-
-
-
 
 module.exports = app;
